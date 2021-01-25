4bb335f0ffa131523efacfc47fa7007160ecfa7f
Akhil Seshan <akhilses@gmail.com>
2021-01-25 21:40:29 +0530

    removed content from boilerplate

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="navbar">
+    <h1>Navbar</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
